a0b348bef67863e4ccb6efa3315a9152465aabc5
Kirill <pn79122821224@gmail.com>
2020-08-17 13:45:58 +0500

    fix(utils): fixes lint issues

--- src/utils.ts
+++ src/utils.ts
@@ -1,32 +1,42 @@
-import { ChangeEvent, DragEvent } from 'react';
-import isAccepted from 'attr-accept';
+import isAccepted from "attr-accept";
+import { ChangeEvent, DragEvent } from "react";
 
-import { MAX_SIZE_ERROR, FILE_TYPE_ERROR } from './constants';
+import { FILE_TYPE_ERROR, MAX_SIZE_ERROR } from "./constants";
 
+export const isDragEvent = (
+  event: ChangeEvent<HTMLInputElement> | DragEvent
+): event is DragEvent => event.hasOwnProperty("dataTransfer");
 
-export const isDragEvent = (event: ChangeEvent<HTMLInputElement> | DragEvent): event is DragEvent => 
-  event.hasOwnProperty('dataTransfer');
-
-type GetFilesFromEvent = (event: ChangeEvent<HTMLInputElement> | DragEvent) => FileList;
-export const getFilesFromEvent: GetFilesFromEvent = (event) => {
+type GetFilesFromEvent = (
+  event: ChangeEvent<HTMLInputElement> | DragEvent
+) => FileList;
+export const getFilesFromEvent: GetFilesFromEvent = event => {
   return isDragEvent(event) ? event.dataTransfer.files : event.target.files;
-} 
+};
 
 type CheckFileSize = (file: File, maxSize?: number) => string | undefined;
 const checkFileSize: CheckFileSize = (file, maxSize) => {
-  if (!maxSize || file.size <= maxSize) { return; }
+  if (!maxSize || file.size <= maxSize) {
+    return;
+  }
 
   return MAX_SIZE_ERROR;
-}
+};
 
 type CheckFileType = (file: File, accept?: string) => string | undefined;
 const checkFileType: CheckFileType = (file, accept) => {
-  if (!accept || isAccepted(file, accept)) { return; }
-  
-  return FILE_TYPE_ERROR;
-}
+  if (!accept || isAccepted(file, accept)) {
+    return;
+  }
 
-type ValidateFile = (params: { file: File; maxSize?: number; accept?: string; }) => string[];
+  return FILE_TYPE_ERROR;
+};
+
+type ValidateFile = (params: {
+  file: File;
+  maxSize?: number;
+  accept?: string;
+}) => string[];
 export const validateFile: ValidateFile = ({ file, maxSize, accept }) => {
   const maxSizeError = checkFileSize(file, maxSize);
   const fileTypeError = checkFileType(file, accept);
